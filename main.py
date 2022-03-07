52a546c6b6e49eb7f1e10fcba5b57c8407d77843
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2022-03-07 11:08:49 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+'''Please write a function named chessboard, which prints out a chessboard made out of ones and zeroes. 
+The function takes an integer argument, which specifies the length of the side of the board. ''' 
+
+
+def chessboard(size):
+    i = 0
+    while i < size:
+        if  i % 2 == 0 :
+            row = "10" * size
+        else :
+            row = "01" * size
+        print(row[0:size])
+        i += 1
+
+
+# Testing the function
+if __name__ == "__main__":
+    chessboard(3)
